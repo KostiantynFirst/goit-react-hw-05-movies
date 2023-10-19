36f0938a29b3dd2982d272146737ddb415709096
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-10-19 21:37:21 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,18 +3,18 @@ import { Routes, Route } from "react-router-dom";
 import { Layout } from "./Layout/Layout";
 
 
-const Gallery = lazy(() => import('./Gallery').then(module => ({
-    ...module,
-    default: module.Gallery,
-}))
-);
+// const Gallery = lazy(() => import('./Gallery').then(module => ({
+//     ...module,
+//     default: module.Gallery,
+// }))
+// );
 
-const Subbreads = lazy(() => import('./Subbreads').then(module => ({
-    ...module,
-    default: module.Subbreads
+// const Subbreads = lazy(() => import('./Subbreads').then(module => ({
+//     ...module,
+//     default: module.Subbreads
 
-}))
-);
+// }))
+// );
 
 const Home = lazy(() => import("../pages/Home/Home"));
 const Movies = lazy(() => import("../pages/Movies/Movies"));
@@ -30,8 +30,8 @@ return (
     <Route index element={<Home />} />
     <Route path="movies" element={<Movies />} />
     <Route path="movies/:movieId" element={<MovieDetails />} >
-      <Route path="subbreads" element={<Subbreads />} />
-      <Route path="gallery" element={<Gallery />} />
+      {/* <Route path="subbreads" element={<Subbreads />} />
+      <Route path="gallery" element={<Gallery />} /> */}
     </Route >
   </Route>
 </Routes>
