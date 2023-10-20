5862986e9dbfd107e80bf1abd6290a9e11528119
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-10-20 16:02:32 +0300

    Update Cast.js

--- src/components/Cast/Cast.js
+++ src/components/Cast/Cast.js
@@ -1,8 +1,32 @@
-
-
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getCast } from "components/movie-api";
 
 const Cast = () => {
 
+    const {movieId} = useParams;
+    const [castData, setCastData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+
+    useEffect(() => {
+        if(!movieId) {
+            return;
+        }
+
+        const getCAstDetails = async () => {
+            
+        }
+
+    })
+
+
+
+
+
+
 }
+
+
 
 export default Cast
