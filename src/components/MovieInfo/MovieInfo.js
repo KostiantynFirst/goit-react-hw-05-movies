1d4ddc8003e9bc5b9f2520a22a39470682748574
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-10-20 22:36:31 +0300

    Update MovieInfo.js

--- src/components/MovieInfo/MovieInfo.js
+++ src/components/MovieInfo/MovieInfo.js
@@ -1,3 +1,4 @@
+import { MovieContainer, PosterImage, MovieDetails, NoImage, MovieTitle, InfoParagraph, OverviewHeading, GenreList, GenreItem } from "./MovieInfo.styled";
 
 export const MovieInfo = ({ movieInfoDetails }) => {
     const { title, original_title, release_date, overview = 'There is no overview', popularity, genres = [], poster_path, status,} = movieInfoDetails;
